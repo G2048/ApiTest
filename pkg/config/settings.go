7835579837182859ae42fc40031c6bb5d02f838b
G2048 <void.infiinity@gmail.com>
2024-08-16 23:48:43 +0700

    feature(config): add short name for cmd options

--- pkg/config/settings.go
+++ pkg/config/settings.go
@@ -4,15 +4,15 @@ import _ "github.com/danielgtaylor/huma/v2/formats/cbor"
 import _ "github.com/fxamacker/cbor/v2"
 
 type AppSettings struct {
-    AppName    string `help:"Application name" default:"ApiTest"`
-    AppVersion string `help:"Application version" default:"1.0.0"`
+    AppName    string `short:"n" help:"Application name" default:"ApiTest"`
+    AppVersion string `short:"v" help:"Application version" default:"1.0.0"`
 }
 type LogSettings struct {
     AppSettings
-    LogLevel string `help:"Log level" default:"debug"`
+    LogLevel string `short:"l" help:"Log level" default:"debug"`
 }
 
 type ServerSettings struct {
     LogSettings
-    Port string `help:"Server port" default:"3333"`
+    Port string `short:"p" help:"Server port" default:"3333"`
 }
